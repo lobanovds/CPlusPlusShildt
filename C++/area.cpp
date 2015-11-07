8a148c6a23a269833c7580d80bdf4cd6fad5a064
Дмитрий Лобанов <lobanovds@gmail.com>
2015-11-08 02:09:13 +0500

    area.cpp

---
+++ C++/area.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int length;
+	int width;
+	int area;
+	 length =5;
+	 width = 7;
+
+	 area = length*width;
+
+	 cout << "area is ";
+	 cout << area;
+
+	return 0;
+}
